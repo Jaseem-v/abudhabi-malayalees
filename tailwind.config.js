4def0f7e469b3123f25743bf3a46eea75dcc7d3a
Jaseem-v <muhammedjaseem753@gmail.com>
2023-03-03 23:35:28 +0530

    new branch frontend added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".*.{html,js}"],
+  content: ["./*.html"],
   theme: {
     extend: {
       backgroundColor: {
@@ -12,7 +12,7 @@ module.exports = {
         logoBg:
           "linear-gradient(180deg, #F5E4BB 50.51%, rgba(255, 166, 0, 0) 151.89%)",
         gold: "linear-gradient(135.12deg, #F8D754 24.95%, #FFF5CF 193.21%)",
-        contactUsImg: "url('./assests/images/contactUsImg.png')",
+        contactUsImg: "url('./assets/images/contactUsImg.png')",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
@@ -29,6 +29,7 @@ module.exports = {
       },
       boxShadow: {
         card: "-7px 7px 29px 1px rgba(0, 0, 0, 0.17)",
+        eventCard: "0px 4px 51px rgba(0, 0, 0, 0.13)",
       },
     },
   },
