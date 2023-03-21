0aada36d6e53b6565fbc2c9c7957451cb7dcb75c
hananAzeez <hananazeez007@gmail.com>
2023-03-21 14:25:20 +0530

    mobile menu

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 function myFunction() {
+  var menuOpen = document.getElementById("menu-open");
+  var menuClose = document.getElementById("menu-close");
   var x = document.getElementById("myLinks");
   if (x.classList.contains("hidden")) {
     x.classList.remove("hidden");
   } else {
     x.classList.add("hidden");
+  }
+  if (menuOpen.classList.contains("hidden")) {
+    menuOpen.classList.remove("hidden");
+    menuClose.classList.add("hidden");
+  } else {
+    menuOpen.classList.add("hidden");
+    menuClose.classList.remove("hidden");
   }
 }
